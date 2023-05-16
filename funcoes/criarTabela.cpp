ee5be9e8451f7d8e6e59e64ea722943c58885313
Victor Reis Freitas <130703266+Vitelfs@users.noreply.github.com>
2023-05-15 22:24:49 -0300

    Update criarTabela.cpp

--- funcoes/criarTabela.cpp
+++ funcoes/criarTabela.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "preencherTabelaLetras.cpp"
 
-void criarTabela(std::string (&tabela)[][30]){ // preenche a tebela com letras aleatorias
+void criarTabela(std::string (*tabela)[30]){ // preenche a tebela com letras aleatorias
     for(int i = 0;i < 30;i++){
         for(int j = 0;j < 30;j++){
             tabela[i][j] = preencherTabelaLetras();      
